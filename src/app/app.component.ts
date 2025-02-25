6b8bc2981335e239122ab7b09993445b65ffee13
fabricio-saa <fabricio_inc27@hotmail.com>
2025-02-25 08:44:24 -0300

    Importing CommonModule to use ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  tasks = [
+    'Install angular CLI',
+    'Reset html',
+    'Say hello world',
+    'Use directives'
+  ];
 }
