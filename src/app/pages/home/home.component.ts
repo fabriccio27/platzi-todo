1a0dda5f4ccffcd902bd64ac9799322c834a4fdc
fabricio-saa <fabricio_inc27@hotmail.com>
2025-03-04 09:23:24 -0300

    Add reactivity to tasks in home.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+ tasks = signal([
+  'Feel better',
+  'Work at your best current capacity',
+  'Rest some'
+ ])
 }
